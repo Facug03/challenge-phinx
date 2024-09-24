d9d4c43a835d1268a2b5e5762840460d7c1bc9b3
Facug03 <facundogonzales1550@gmail.com>
2024-09-24 20:44:40 -0300

    feat: finish ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Stack, Typography } from '@mui/material'
+import useSWR from 'swr'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { PokemonCard } from '@/src/components/pokemon-card'
+import { Battle } from '@/src/components/battle'
+import { fetcher } from '@/src/services/fetcher'
+import { Pokemon } from '@/src/models/pokemon'
+import { AppSkeleton } from '@/src/skeletons/app-skeleton'
+
+export function App() {
+  const { data, error, isLoading } = useSWR<Pokemon[]>('/pokemon', fetcher)
+  const [battle, setBattle] = useState<{
+    fighterOne?: Pokemon
+    fighterTwo?: Pokemon
+  } | null>(null)
+
+  if (isLoading) return <AppSkeleton />
+
+  if (error || !data) return <Typography variant='h5'>Error: failed to load pokemons</Typography>
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <Stack component='main' sx={{ maxWidth: '1280px', margin: '2rem auto' }}>
+      <Typography variant='h4' component='h1' marginBottom='2rem'>
+        Battle of Pokemon
+      </Typography>
+
+      <Typography variant='h5' component='h2' marginBottom='1rem'>
+        Select your pokemon
+      </Typography>
+
+      <Stack
+        sx={{
+          flexWrap: 'wrap',
+          flexDirection: 'row',
+          gap: '1rem',
+          marginBottom: '2rem',
+          justifyContent: { xs: 'center', sm: 'flex-start' }
+        }}
+      >
+        {data.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.id}
+            pokemon={pokemon}
+            type='default'
+            onClick={() => {
+              const removeSelected = data.filter((p) => p.id !== pokemon.id)
+              const randomOpponent = removeSelected[Math.floor(Math.random() * removeSelected.length)]
+
+              setBattle({ fighterOne: pokemon, fighterTwo: randomOpponent })
+            }}
+          />
+        ))}
+      </Stack>
+
+      {Boolean(battle && battle?.fighterOne && battle.fighterTwo) && (
+        <Battle fighterOne={battle!.fighterOne!} fighterTwo={battle!.fighterTwo!} />
+      )}
+    </Stack>
   )
 }
-
-export default App
