cf100ab445b9834cd5df2e41a9fbcfbaa5a2c240
Facug03 <facundogonzales1550@gmail.com>
2024-09-24 14:45:17 -0300

    feat: add fight logic

--- api/src/modules/battle/battle.service.ts
+++ api/src/modules/battle/battle.service.ts
@@ -1,13 +1,58 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
 import { CreateBattleDto } from './dto/create-battle.dto'
+import { Battle } from './entities/battle.entity'
 
 @Injectable()
 export class BattleService {
+  constructor(@InjectRepository(Battle) private battleRepository: Repository<Battle>) {}
+
   create(createBattleDto: CreateBattleDto) {
-    return 'This action adds a new battle'
+    const { fighterOne, fighterTwo } = createBattleDto
+
+    const firstTurn = this.getFirstTurn(createBattleDto)
+    const winnerId = this.fight(createBattleDto, firstTurn)
+
+    return this.battleRepository.save({
+      winner: {
+        id: winnerId
+      },
+      loser: {
+        id: winnerId === fighterOne.id ? fighterTwo.id : fighterOne.id
+      }
+    })
   }
 
   findAll() {
-    return 'This action returns all battle'
+    return this.battleRepository.find({
+      relations: ['winner', 'loser']
+    })
+  }
+
+  fight(fighters: CreateBattleDto, turn: string): string {
+    const { fighterOne, fighterTwo } = fighters
+    const pokemonAttacking = fighterOne.id === turn ? fighterOne : fighterTwo
+    const pokemonDefending = fighterOne.id === turn ? fighterTwo : fighterOne
+
+    const damage = pokemonAttacking.attack - pokemonDefending.defense
+    pokemonDefending.hp -= damage
+    console.log({ pokemonDefending })
+
+    if (pokemonDefending.hp > 0) return this.fight(fighters, pokemonDefending.id)
+
+    console.log({ pokemonAttacking })
+    return pokemonAttacking.id
+  }
+
+  getFirstTurn(fighters: CreateBattleDto): string {
+    const { fighterOne, fighterTwo } = fighters
+
+    if (fighterOne.speed === fighterTwo.speed) {
+      return fighterOne.attack > fighterTwo.attack ? fighterOne.id : fighterTwo.id
+    }
+
+    return fighterOne.speed > fighterTwo.speed ? fighterOne.id : fighterTwo.id
   }
 }
