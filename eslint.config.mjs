24762c589f9c9beb2368cb4370076fa0811347f6
Facug03 <facundogonzales1550@gmail.com>
2024-09-24 12:11:52 -0300

    chore: finish config of prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,33 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default tseslint.config([
+const compat = new FlatCompat()
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...compat.extends('eslint-config-standard'),
   {
-    files: ['/web/**/*.{ts,tsx}', '/api/**/*.{ts,tsx}'],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier', 'standard'],
+    languageOptions: {
+      parser: tseslint.parser
+    },
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
+      indent: ['error', 2],
       semi: ['warn', 'never'],
-      'no-eval': 'off',
       'no-undef': 'off',
       'no-use-before-define': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'space-before-function-paren': 'off',
-      'no-useless-constructor': 'off'
+      'no-useless-constructor': 'off',
+      quotes: ['warn', 'single'],
+      '@stylistic/indent': ['error', 2]
     }
   },
   {
@@ -38,6 +51,7 @@ export default tseslint.config([
   {
     files: ['/api/**/*.{ts,tsx}'],
     rules: {
+      indent: ['error', 2],
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -48,10 +62,11 @@ export default tseslint.config([
         ...globals.node,
         ...globals.jest
       },
-      parsertOptions: {
+      parserOptions: {
         project: './api/tsconfig.json',
         sourceType: 'module'
       }
     }
-  }
-])
+  },
+  eslintConfigPrettier
+)
