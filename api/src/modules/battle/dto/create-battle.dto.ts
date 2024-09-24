fef73b4d5fa3cd2e075aa5e9866c6a06ec8ed327
Facug03 <facundogonzales1550@gmail.com>
2024-09-24 14:44:51 -0300

    feat: add create battle dto

--- api/src/modules/battle/dto/create-battle.dto.ts
+++ api/src/modules/battle/dto/create-battle.dto.ts
@@ -1,9 +1,33 @@
-import { IsString } from 'class-validator'
+import { Type } from 'class-transformer'
+import { IsInt, IsString, Min, ValidateNested } from 'class-validator'
+
+class PokemonStatsDto {
+  @IsString()
+  id: string
+
+  @IsInt()
+  @Min(0)
+  attack: number
+
+  @IsInt()
+  @Min(0)
+  defense: number
+
+  @IsInt()
+  @Min(0)
+  hp: number
+
+  @IsInt()
+  @Min(0)
+  speed: number
+}
 
 export class CreateBattleDto {
-  @IsString()
-  fighterOneId: string
+  @ValidateNested()
+  @Type(() => PokemonStatsDto)
+  fighterOne: PokemonStatsDto
 
-  @IsString()
-  fighterTwoId: string
+  @ValidateNested()
+  @Type(() => PokemonStatsDto)
+  fighterTwo: PokemonStatsDto
 }
