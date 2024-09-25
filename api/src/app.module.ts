9fa11d7d8d7344acd3d145c771fe5cb52bac38af
Facug03 <facundogonzales1550@gmail.com>
2024-09-25 10:21:02 -0300

    feat: add health module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { PokemonModule } from '@/src/modules/pokemon/pokemon.module'
 import { BattleModule } from '@/src/modules/battle/battle.module'
 import { configTypeOrm } from '@/src/config/type-orm'
+import { HealthModule } from '@/src/modules/health/health.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configTypeOrm), PokemonModule, BattleModule]
+  imports: [TypeOrmModule.forRoot(configTypeOrm), PokemonModule, BattleModule, HealthModule]
 })
 export class AppModule {}
