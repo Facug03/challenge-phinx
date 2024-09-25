4c2d766dd0cc6ab5c21316b584983df6a3d86016
Facug03 <facundogonzales1550@gmail.com>
2024-09-25 10:29:24 -0300

    feat: change name of database

--- api/src/config/type-orm.ts
+++ api/src/config/type-orm.ts
@@ -2,7 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 
 export const configTypeOrm: DataSourceOptions = {
   type: 'sqlite',
-  database: 'db/db.sqlite',
+  database: 'db/database.sqlite',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   synchronize: true
